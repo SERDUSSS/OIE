ad68cc3bd62f85adbeaa564c73b524f9c1d9e47a
serdusone <serdusone@gmail.com>
2022-12-12 20:54:31 +0100

    new file:   AVANZADO/.vscode/c_cpp_properties.json 	new file:   AVANZADO/.vscode/launch.json 	new file:   AVANZADO/.vscode/settings.json 	new file:   AVANZADO/B_BitProblem.cpp

---
+++ AVANZADO/B_BitProblem.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int iteraciones;
+    cin >> iteraciones;
+    for (int i = 0; i < iteraciones; i++)
+    {
+        
+    }
+}
