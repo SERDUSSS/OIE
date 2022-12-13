69bfdb2b38be9df71152455bfe56fe567a0dd1a1
serdusone <serdusone@gmail.com>
2022-12-13 19:53:38 +0100

    new file:   AVANZADO/A_schoolDance 	modified:   AVANZADO/A_schoolDance.cpp

--- AVANZADO/A_schoolDance.cpp
+++ AVANZADO/A_schoolDance.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main()
@@ -10,13 +11,14 @@ int main()
     cin >> chicas;
     cin >> pares;
     int total_pares, a, b;
+    string output;
     for (int i = 0; i < pares; i++)
     {
         cin >> a;
         cin >> b;
-        if (a == chicos or b == chicas) total_pares++;
+        if (a == chicos or b == chicas) {total_pares++; output += to_string(a) + ' ' + to_string(b) + '\n';}
 
     }
-    cout << total_pares << '\n' << ;
+    cout << total_pares << '\n' << output;
     return 0;
 }
